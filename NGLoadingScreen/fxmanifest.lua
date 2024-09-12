365581f5003f7cb019759c86d4229b3ed26185bd
T-R-X-S-H-Dev <trxshtv2001@gmail.com>
2024-09-11 21:05:38 -0400

    Add files via upload

---
+++ NGLoadingScreen/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version('cerulean')
+games({ 'gta5' })
+
+autor 'T-R-X-S-H'
+version '1.0.0'
+
+lua54 'yes'
+
+loadscreen 'Interface/NGLoadingScreen.html'
+
+files({
+  'Interface/NGLoadingScreen.html',
+  'Interface/css/style.css',
+  'Interface/src/index.js',
+  'Interface/assets/*.png',
+  'Interface/assets/*.jpg',
+  'Interface/assets/*.svg'
+})
